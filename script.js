0c0710ca3314d7569af8f645d0d962b717404ba0
Dineshk-2024 <dinesh42002@mail.com>
2024-08-07 10:18:52 +0530

    st

---
+++ script.js
@@ -0,0 +1,21 @@
+let fan = document.getElementById("fan");
+
+function myfunon() {
+    fan.style.animationDuration = "3s";
+}
+
+function myfunoff() {
+    fan.style.animationDuration = "0s";
+}
+
+function myfun2() {
+    fan.style.animationDuration = "1s";
+}
+
+function myfun3() {
+    fan.style.animationDuration = "0.5s";
+}
+
+function myfun4() {
+    fan.style.animationDuration = "0.1s";
+}
